471a265b8289d8c142b9dd6646b1233f7b068e0a
Mark Shepherd <markshep1@hotmail.com>
2022-07-10 19:09:09 +0100

    Complete implementation of task 05 and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    
+    if (num1 > num2) {
+        let newNum1 = num2;
+        let newNum2 = num1;
+        let total = 0;
+        for (let i = newNum1; i < newNum2+1; i++) {
+            total += i;
+        }
+        return total;
+    }
+    else {
+        let total = 0;
+        for (let i = num1; i < num2+1; i++) {
+            total += i;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line

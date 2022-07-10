26fb518b1640f4890a256be922e5d192cc3b3d06
Mark Shepherd <markshep1@hotmail.com>
2022-07-10 21:21:06 +0100

    Complete implementation of task 07 and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fDegrees) {
+  let celcius = Math.round(((fDegrees - 32) * (5/9)) * 10) / 10;
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function(cDegrees) {
+  let fahrenheit = Math.round((cDegrees * (9/5) + 32) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line

c3bf2a0ac71539af087e3cb1c5e678fde1e79c56
Mark Shepherd <markshep1@hotmail.com>
2022-07-10 15:02:35 +0100

    Complete implementation of task 03 and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
 
+const reverseString = function(string) {
+    let j = -1;
+    let new_string = '';
+    for (let i = 0; i < (string.length); i++) {
+        if (j === -1) {
+            new_string += string.slice(j);
+            j--;
+        }
+        else {
+            new_string += string.slice(j, j+1);
+            j--;
+        }
+
+    }
+    return new_string;
 };
 
 // Do not edit below this line

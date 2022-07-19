2432a3c5f6435164b0191ce57c49b954fe52b6bc
Mark Shepherd <markshep1@hotmail.com>
2022-07-19 17:59:53 +0100

    Complete task 08 and pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let total = a + b;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let total = a - b;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n === 0) {
+    return 1;
+  }
+  let total = 1;
+  for (let i = n; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line

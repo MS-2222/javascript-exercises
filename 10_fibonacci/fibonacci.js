b921810f56abc0611810371c6437ce864f583748
Mark Shepherd <markshep1@hotmail.com>
2022-07-24 08:25:19 +0100

    Complete task 10 and pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function fib(num) {
+    if(num < 0) {
+        let n = 'OOPS';
+        return n;
+    }
 
-};
+    let n = Math.floor((((1 + Math.sqrt(5)) / 2)**num - ((1 - Math.sqrt(5)) / 2)**num) / (Math.sqrt(5)));
+    
+    return n;
+}
+
 
 // Do not edit below this line
 module.exports = fibonacci;
